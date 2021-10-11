65fbbd07486e78bfce15a11fbb8b3eaa24824f0c
tajkirkpatrick <50558516+tajkirkpatrick@users.noreply.github.com>
2021-10-10 23:28:08 -0500

    Base Files

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM golang:alpine as builder
+
+WORKDIR /app
+
+COPY . .
+
+RUN go mod download
+
+COPY *.go .
+
+RUN go build -o ./api-server
+
+EXPOSE 8080
+
+CMD ["/api-server"]
+
+# Minimize Container
+
+FROM alpine:latest
+
+WORKDIR /go/bin/api-server
+
+RUN apk --no-cache add curl
+
+COPY --from=builder /app/api-server .
+
+HEALTHCHECK CMD curl --fail http://localhost:8080/healthcheck || exit 1
+
+CMD ["./api-server"]
---
+++ server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// Handler
+func homePage(c echo.Context) error {
+  title := "Connected to Homepage"
+  return c.String(http.StatusOK, title)
+}
+
+func gotify(c echo.Context) error {
+	// Get title and message from the query string
+	title := c.QueryParam("title")
+	message := c.QueryParam("message")
+  token := c.QueryParam("token")
+
+  http.PostForm("https://push.ldsnetwork.xyz/message?token=" + token,
+    url.Values{"message": {message}, "title": {title}})
+
+	return c.String(http.StatusOK, "title:" + title + ", message:" + message)
+}
+
+func main() {
+  // Echo instance
+  e := echo.New()
+
+  // Middleware
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+
+  // Routes
+  e.GET("/", homePage)
+  e.GET("/gotify", gotify)
+
+  // Healthcheck Endpoint For Docker Container
+  e.GET("/healthcheck", func(c echo.Context) error {
+    return c.NoContent(http.StatusOK)
+  })
+
+  // Start server
+  e.Logger.Fatal(e.Start(":8080"))
+}
+
